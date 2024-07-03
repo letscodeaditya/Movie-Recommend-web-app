0db2be9b50364329d3b1bdad2c7a6cc402b3e634
letscodeaditya <whyayush24@gmail.com>
2024-07-03 13:03:11 +0530

    dev

--- Client/src/store/Movie-store.jsx
+++ Client/src/store/Movie-store.jsx
@@ -70,8 +70,6 @@ const MovieStoreProvider = ({ children }) => {
       });
   };
 
-  console.log(apiKey);
-  console.log(apiUrl);
   useEffect(() => {
     fetchData("/movie/popular?language=en-US&page=1", setMovieList);
     fetchData("/tv/popular?language=en-US&page=1", setTvList);
